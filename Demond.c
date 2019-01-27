1c9c3af6e5855b8616a64fe2d229b5738f5e5bb0
lichao252244354 <lichao1163928616@163.com>
2019-01-27 14:57:08 +0800

    add Demond.c

---
+++ Demond.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+int main(void)
+{
+	pid_t pid,sid;
+	pid = fork();
+	if(pid > 0)
+	{
+		return 0;
+	}
+	sid = setsid();
+	int reval = chdir("/home");
+	if(reval != 0)
+	{
+		perror("chdir falied");
+		exit(1);
+	}
+	printf("sucess changed\n");
+	umask(0002);
+	printf("sucess\n");
+	printf("222\n");
+	close(STDIN_FILENO);
+	int fd = open("/dev/null",O_RDWR);
+	dup2(fd,STDOUT_FILENO);
+	dup2(fd,STDERR_FILENO);
+	while(1)
+	{
+
+	}
+
+
+	return 0;
+}
